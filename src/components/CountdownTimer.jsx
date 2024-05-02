e7138609c89efda94ea8680275bd6fe53b52060c
Kirill Shpak <kirillshpak7212@gmail.com>
2024-05-02 10:08:31 +0500

    Added CountdownTimer

--- src/components/CountdownTimer.jsx
+++ src/components/CountdownTimer.jsx
@@ -1,9 +1,26 @@
 import React, { useState, useEffect } from 'react';
 
 const CountdownTimer = () => {
+    const [timer, setTimer] = useState(10)
+
+    useEffect(()=> {
+        const countTimer = setTimeout(() => {
+            if (timer === 0) {
+                clearTimeout(countTimer);
+            } else {
+                setTimer((timer) => timer - 1)
+            }
+        }, 200);
+
+        return () => {
+            clearTimeout(countTimer)
+        };
+
+    }, [timer]);
+
     return (
         <>
-        
+            <p>{timer}</p>
         </>
     )
 };
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
 const UserList = () => {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        
+    })
+
     return (
         <>
         
