400d593d8b5ba49acf93cd2002029cb901e1498c
= <=>
2024-05-02 18:43:07 +0300

    Added WindowSize and UserList

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,16 +1,49 @@
 import React, { useState, useEffect } from 'react';
 
+const UserShow = ({id, name, username, email, street, suite, city, zipcode, lat, lng}) => {
+    return (
+        <div className='userBlock'>
+            <div><h1>ID</h1><p>{id}</p></div>
+            <div><h1>Name</h1><p>{name}</p></div>
+            <div><h1>Username</h1><p>{username}</p></div>
+            <div><h1>Email</h1><p>{email}</p></div>
+            <div><h1>Address</h1><p>{street}, {suite}, {city}, {zipcode}, {lat}, {lng}</p></div>
+        </div>
+    )
+}
+
 const UserList = () => {
-    const [data, setData] = useState([])
+    const [users, setUsers] = useState([])
 
     useEffect(() => {
-        
-    })
+        fetch('https://jsonplaceholder.typicode.com/users')
+          .then(response => response.json())
+          .then(data => setUsers(data))
+          .catch(error => console.error(error));
+    }, []);
 
     return (
-        <>
-        
-        </>
+    <div className='userSection'>
+      <h1>User List</h1>
+        <ul className='userList'>
+            {users.map(user => (
+            <li key={user.id}>
+                <UserShow 
+                    id={user.id}
+                    name={user.name}
+                    username={user.username}
+                    email={user.email}
+                    street={user.address.street}
+                    suite={user.address.suite}
+                    city={user.address.city}
+                    zipcode={user.address.zipcode}
+                    lat={user.address.geo.lat}
+                    lng={user.address.geo.lng}
+                />
+            </li>
+            ))}
+        </ul>
+    </div>
     )
 };
 
--- src/components/WindowSize.jsx
+++ src/components/WindowSize.jsx
@@ -1,11 +1,35 @@
 import React, { useState, useEffect } from 'react';
 
 const WindowSize = () => {
-    return (
-        <>
-        
-        </>
-    )
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+
+  const handleResize = () => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <div className='windowBlock'>
+      <h1>Window Size</h1>
+      <div>
+        <p>Width: {windowSize.width}px</p>
+        <p>Height: {windowSize.height}px</p>
+      </div>
+    </div>
+  );
 };
 
 export default WindowSize;
